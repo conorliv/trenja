74c69723a76f4cee8660b01343ae1d2325bb527d
Conor Livingston <conorliv@gmail.com>
2015-04-10 00:35:14 -0400

    Update kanji controller

--- app/controllers/kanji_controller.rb
+++ app/controllers/kanji_controller.rb
@@ -1,5 +1,35 @@
 class KanjiController < ApplicationController
-    def index
+  def index
+    @kanji = Kanji.all.sort_by { |kanji| kanji.count }.reverse
+  end
 
-    end
+  def update_counts
+    tweets = TwitterAPIClient.get_tweets('urawillow')
+    puts tweets
+    Kanji.process_tweets(tweets)
+    # Source.process_tweets(tweets)
+  end
+
+  #def write(counts, sentences)
+  #  counts.each do |kanji,count|
+  #    candidates = []
+  #    #Go through the sentences array
+  #    sentences.each do |sentence|
+  #      candidates << sentence if sentence.include?(kanji)
+  #    end
+
+  #    #Sort the collected sentences by length.
+  #    candidates.sort{ |a,b| a.length <=> b.length }
+  #    three_longest = []
+  #    candidates[0..2].each do |element|
+  #      three_longest << element
+  #    end
+
+  #    three_longest.each do |sentence|
+  #     kanji_made_bold = sentence.gsub(kanji,"<strong>#{kanji}</strong>")
+  #     username_removed = kanji_made_bold.gsub(/@[a-zA-Z0-9_]+/,'')
+  #     url_removed = username_removed.gsub(/http:[a-zA-Z0-9\/\.]*/,'')
+  #    end
+  #  end
+  #end
 end
