7cbd6413b7327a6a26727f290cf37450ce9e3c50
Conor Livingston <conorliv@gmail.com>
2015-07-24 21:07:44 -0400

    Cleanup kanji controller

--- app/controllers/kanji_controller.rb
+++ app/controllers/kanji_controller.rb
@@ -1,4 +1,4 @@
-class KanjiController < ApplicationController
+class KanjiController < ApplicationController)
   def index
     @kanji = Kanji.all.sort_by { |kanji| kanji.count }.reverse
   end
@@ -22,29 +22,6 @@ class KanjiController < ApplicationController
   private
 
   def get_random_source
-    Source.find_by_id(Source.count)
+    Source.find_by_id(rand(0..Source.count))
   end
-
-  #def write(counts, sentences)
-  #  counts.each do |kanji,count|
-  #    candidates = []
-  #    #Go through the sentences array
-  #    sentences.each do |sentence|
-  #      candidates << sentence if sentence.include?(kanji)
-  #    end
-
-  #    #Sort the collected sentences by length.
-  #    candidates.sort{ |a,b| a.length <=> b.length }
-  #    three_longest = []
-  #    candidates[0..2].each do |element|
-  #      three_longest << element
-  #    end
-
-  #    three_longest.each do |sentence|
-  #     kanji_made_bold = sentence.gsub(kanji,"<strong>#{kanji}</strong>")
-  #     username_removed = kanji_made_bold.gsub(/@[a-zA-Z0-9_]+/,'')
-  #     url_removed = username_removed.gsub(/http:[a-zA-Z0-9\/\.]*/,'')
-  #    end
-  #  end
-  #end
 end
