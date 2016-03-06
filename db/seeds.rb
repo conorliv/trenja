22ddbe21cd832de9825b0044fb5cf6460adc38b9
Conor Livingston <conorliv@gmail.com>
2016-03-06 18:45:57 -0500

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
 Source.delete_all
-Review.delete_all
 
 Source.create!(name: 'makoJOSIAH', value_index: 5, source_type: 'twitter')
-
-Kanji.create!([
-  { kanji: '車', count: 10 },
-  { kanji: '今', count: 5 },
-  { kanji: '馬', count: 4 },
-  { kanji: '日', count: 3 }
-])
 
