aec46a5807eb9ed3b6e92199463ca27dec5bec24
Conor Livingston <conorliv@gmail.com>
2016-03-06 12:38:43 -0500

    Update source import service spec

--- spec/services/source_import_service_spec.rb
+++ spec/services/source_import_service_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe SourceImportService do
   describe '#import' do
-    let(:source_1) { 'FakeSource' }
-    let(:source_2) { 'FakeSource2' }
+    let(:source_1) { double('FakeSource', screen_name: 'Fake1')  }
+    let(:source_2) { double('FakeSource2', screen_name: 'Fake2') }
     let(:sources) { [source_1, source_2] }
 
     it 'loads sources into DB' do
       SourceImportService.import(sources)
 
       expect(Source.count).to eq 2
-      expect(Source.first.name).to eq source_1
+      expect(Source.first.name).to eq source_1.screen_name
     end
   end
 end
