92b167526fc2baae5c8711e448aca7845eb14f04
Conor Livingston <conorliv@gmail.com>
2016-03-06 19:33:14 -0500

    Revert "Debug require statement"

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,4 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require File.expand_path('../initializers/debug_require', __FILE__)
-
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
